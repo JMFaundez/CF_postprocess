5888df26d97f1f0700091e7345a11bc759df54af
Jose Faundez-Alarcon <josfa@mech.kth.se>
2020-03-07 20:12:29 +0100

    ..

--- generate_plots.m
+++ generate_plots.m
@@ -43,6 +43,20 @@ for i=1:6
     end
 end
 
+[ny,nx] = size(Xc{1});
+% find BL thickness
+U_inf = 1;
+ind_bl = zeros(6,1);
+for i=1:6
+    U_av = mean(Uc{i},2);
+    for j=1:length(U_av)
+        if U_av(j)>=0.99*U_inf
+            ind_bl(i) = j;
+            break
+        end
+    end
+end
+
 
 for plotId=1:6
     X = Xc{plotId};
@@ -51,21 +65,26 @@ for plotId=1:6
     u = uc{plotId};
     Y = Y -min(Y(:));
     figure(2000)
+    sgtitle('Tangent Perturbation')
     subplot(3,2,plotId)
+    hold on
     contourf(X,Y,u,'LineStyle','none')
+    plot(X(1,:),Y(ind_bl(plotId),:),'r-')
     xlabel('Span')
     ylabel('Normal')
-    ylim([0, 0.02])
+    ylim([0, 0.01])
     title([x(plotId,:),'% chord'])
+    hold off
     %caxis([minu maxu])
     colorbar()
     
     figure(2001)
+    sgtitle('Tangent Velocity')
     subplot(3,2,plotId)
     contourf(X,Y,U,'LineStyle','none')
     xlabel('Span')
     ylabel('Normal')
-    ylim([0, 0.02])
+    ylim([0, 0.01])
     %caxis([minU maxU])
     title([x(plotId,:),'% chord'])
     colorbar()
